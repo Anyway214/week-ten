5cbce96623680dd876b06d37bc8ad6d958690e69
Anyway214 <42959875+Anyway214@users.noreply.github.com>
2018-10-23 15:50:01 +0800

    Please accept my fix!

--- components/App.js
+++ components/App.js
@@ -23,7 +23,8 @@ export default {
     };
   },
   methods: {
-    updateCurrentChart: function(newChart) {}
+    updateCurrentChart: function(newChart) {},
+    veryNewFunction: function(newChart) {}
   },
   firebase: {
     charts: {
